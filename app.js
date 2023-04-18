34091cb45ff74c812895809b7bbc3e899b3fb665
Dani Bravo <dbravojuanico@icloud.com>
2023-04-18 12:10:27 +0200

    done

---
+++ app.js
@@ -0,0 +1,23 @@
+// first thing we need is express
+const express = require("express")
+const app = express()
+
+app.get("/", (req, res)=>{
+    res.sendFile(__dirname + "/views/home.html")
+})
+
+app.get("/about", (req, res)=>{
+    res.sendFile(__dirname + "/views/about.html")
+})
+
+app.get("/works", (req, res)=>{
+    res.sendFile(__dirname + "/views/works.html")
+})
+
+app.get("/photo-gallery", (req, res)=>{
+    res.sendFile(__dirname + "/views/photo-gallery.html")
+})
+
+app.listen(3000, () => {
+    console.log("listening on port 3000!")
+})
