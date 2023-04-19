f14cf02f8850f0890450cc2f2904f3a194cf0e10
Dani Bravo <dbravojuanico@icloud.com>
2023-04-19 16:47:42 +0200

    correcting paths

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // first thing we need is express
 const express = require("express")
 const app = express()
+app.use(express.static('public'))
 
 app.get("/", (req, res)=>{
     res.sendFile(__dirname + "/views/home.html")
